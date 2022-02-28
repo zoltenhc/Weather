1941219f14e9fb2becefc2fd1e11d1a69785105d
zoltenhc <zoltenhc@gmail.com>
2022-02-28 09:50:58 +0100

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Zoltán Gál on 2022. 02. 28..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
